c22195118b454b6aa631d47988552311716374e3
vadimgaiseniuk <gaisenyukv@gmail.com>
2022-10-28 13:21:38 +0300

    Finished

--- Lab5_Task_1/Program.cs
+++ Lab5_Task_1/Program.cs
@@ -1,58 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-///
-Console.WriteLine("Lab5 C# ");
-AnyFunc();
+﻿namespace Lab5_Task_1;
 
-/// <summary>
-/// 
-///  Top-level statements must precede namespace and type declarations.
-/// At the top-level methods/functions can be defined and used
-/// На верхньому рівні можна визначати та використовувати методи/функції
-/// </summary>
-void AnyFunc()
+public static class Program
 {
-    Console.WriteLine(" Some function in top-level");
-}
-Console.WriteLine("Problems 1 ");
-AnyFunc();
-//  приклад класів
-UserClass cl = new UserClass();
-cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new();
-cl2.Name = " UserClass namespace User ";
-
-
-
-
-/// <summary>
-/// 
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
-/// 
-/// </summary>
-
-namespace User
-{
-    class UserClass
+    public static void Main()
     {
-        public string Name { get; set; }
-       public  UserClass()
-        {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
-        }
+        Person person = new Person("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19);
+        Employee employee = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.1);
+        Worker worker = new Worker("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.6, "Microsoft");
+        Engineer engineer = new Engineer("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 1.4, "Bachelor");
+        
+        person.Show();
+        Console.WriteLine("---------------------------------------------");
+        employee.Show();
+        Console.WriteLine("---------------------------------------------");
+        worker.Show();
+        Console.WriteLine("---------------------------------------------");
+        engineer.Show();
+        Console.WriteLine("---------------------------------------------");
     }
-
-}
-class UserClass
-{
-    public string Name { get; set; }
 }
