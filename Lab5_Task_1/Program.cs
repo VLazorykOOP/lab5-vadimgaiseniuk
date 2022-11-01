925ae7b19e09a29137f6717d905fa74274d264af
vadimgaiseniuk <gaisenyukv@gmail.com>
2022-11-01 13:41:37 +0200

    Correcting

--- Lab5_Task_1/Program.cs
+++ Lab5_Task_1/Program.cs
@@ -4,10 +4,10 @@ public static class Program
 {
     public static void Main()
     {
-        Person person = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 0);
-        Person employee = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.1);
-        Person worker = new Worker("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.6, "Microsoft");
-        Person engineer = new Engineer("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 1.4, "Bachelor");
+        var person = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 0);
+        var employee = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.1);
+        var worker = new Worker("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.6, "Microsoft");
+        var engineer = new Engineer("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 1.4, "Bachelor");
         
         person.Show();
         Console.WriteLine("---------------------------------------------");
--- Lab5_Task_2/Program.cs
+++ Lab5_Task_2/Program.cs
@@ -4,10 +4,10 @@ public static class Program
 {
     public static void Main()
     {
-        Person person = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 0);
-        Person worker = new Worker("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.6, "Microsoft");
-        Person engineer = new Engineer("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 1.4, "Bachelor");
-        Person employee = new Employee();
+        var person = new Employee("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 0);
+        var worker = new Worker("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 2.6, "Microsoft");
+        var engineer = new Engineer("Vadim", "Gaiseniuk", "Anatoliyovich", "Male", 19, 1.4, "Bachelor");
+        var employee = new Employee();
         
         person.Show();
         Console.WriteLine("---------------------------------------------");
